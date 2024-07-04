96f6f0d3b06f3b14a1b53e1b991e096391bb59b8
farad-alam <farad.alam1997@gmail.com>
2024-07-04 21:28:16 +0600

    saved todo data in json file

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json, os
 
 app = Flask(__name__)
 
-task_details = {}
+def load_tasks():
+    if not os.path.exists('tasks.json'):
+        return {}
+    
+    with open('tasks.json', 'r') as f:
+        try:
+            return json.load(f)
+        except json.JSONDecodeError:
+            return {}
 
-task_id=1
+def save_task(task):
+    with open("tasks.json", "w") as f:
+        json.dump(task, f, indent=4)
+
+task_details = load_tasks()
+# print(task_details)
+
+task_id = max(map(int, task_details.keys()), default=0) + 1 if task_details else 1
 
 @app.route('/')
 def home():
@@ -18,13 +34,18 @@ def add_task():
     time = request.form.get('time')
 
     if task_title and description and time:
-        task_details[task_id] = {"title":task_title,"description":description,"time":time}
+        task_details[str(task_id)] = {"title":task_title,"description":description,"time":time}
         task_id +=1
+        # print(task_details)
+        save_task(task_details)
+
     return redirect(url_for('home'))
 
 @app.route("/task/<int:task_id>")
 def task_detail(task_id):
-    task = task_details.get(task_id)
+    task = task_details.get(str(task_id))
+    # print(task_details)
+    # print(task)
     if not task:
         return "Task not Found", 404
     return render_template("details.html", task=task)
